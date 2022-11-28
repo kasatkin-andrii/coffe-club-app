707f4b4082b9a134906d56d0c1b04ec386f78066
andrii <107546941+kasatkin-andrii@users.noreply.github.com>
2022-11-28 10:48:30 +0200

    Setup navigation
    Setup navigation stack and bottom tab navigation

---
+++ src/navigation/BottomTabNavigator.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeStack from './HomeStack';
+import FavoriteScreen from '../screens/FavoriteScreen';
+import BagScreen from '../screens/BagScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+
+const Tab = createBottomTabNavigator();
+
+const BottomTabNavigator = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name='HomeStack' component={HomeStack}/>
+      <Tab.Screen name='FavoriteScreen' component={FavoriteScreen}/>
+      <Tab.Screen name='BagScreen' component={BagScreen}/>
+      <Tab.Screen name='ProfileScreen' component={ProfileScreen}/>
+    </Tab.Navigator>
+  )
+}
+
+export default BottomTabNavigator
